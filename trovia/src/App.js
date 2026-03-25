f1721e84f4b91ca4ae0a498c21ae84aebf44c865
askmeanythin <priyanshu20012006@gmail.com>
2026-03-25 12:37:39 +0530

    Bug fix

--- trovia/src/App.js
+++ trovia/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef } from 'react'
 import gsap from 'gsap'
 import { MotionPathPlugin } from 'gsap/MotionPathPlugin'
 import ElectricBorder from './ElectricBorder'
